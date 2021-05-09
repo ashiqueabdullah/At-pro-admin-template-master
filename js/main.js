e3fd9e7ebc25cf7fca23ace7a597f2686fab07aa
ashique13 <mdashiqueabdullah@gmail.com>
2021-05-10 01:42:08 +0600

    Header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-$(document).ready(function () {
 
 
-});
+window.onclick=function(event){
+    openCloseDropdown(event)
+}
+
+function closeAlldropdown(){
+    var dropdown=document.getElementsByClassName('dropdown-expand');
+    for(i=0; i<dropdown.length; i++){
+        dropdown[i].classList.remove('dropdown-expand');
+    }
+}
+
+
+
+function openCloseDropdown(event){
+    if(!event.target.matches('.dropdown-toggle')){
+        closeAlldropdown();
+    }else{
+        var toggle=event.target.dataset.toggle;
+        var content=document.getElementById(toggle);
+        if(content.classList.contains('dropdown-expand')){
+            closeAlldropdown()
+            content.classList.remove('dropdown-expand');
+        }else{
+            closeAlldropdown()
+            content.classList.add('dropdown-expand');
+        }
+    }
+}
